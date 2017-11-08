7623582a0190da70873393cae81ae27f15c63803
Parker King-Fournier <parker.k.fournier@gmail.com>
2017-11-08 15:42:36 -0500

    Update pickle_data.py

--- preprocess/pickle_data.py
+++ preprocess/pickle_data.py
@@ -2,10 +2,9 @@ import numpy as np
 import pickle
 
 #import the data and save as a numpy pickle array for fastness
+
 #for x_train
-
 print "\nreading train_x..."
-
 x = np.loadtxt("../data/train_x.csv", delimiter=",") # load from text
 x = x.reshape(-1,64,64)
 x_out = open('x_train_data.pkl', 'wb')
@@ -13,7 +12,6 @@ pickle.dump(x, x_out)
 x_out.close()
 
 print "reading train_y..."
-
 #for y_train
 y = np.loadtxt("../data/train_y.csv", delimeter=",")
 y = y.reshape(-1,1)
@@ -22,7 +20,6 @@ pickle.dump(y, y_out)
 y_out.close()
 
 print "reading test_x...\n"
-
 #for x_test
 t = np.loadtxt("../data/test_x.csv", delimeter=",")
 t = t.reshape(-1,64,64)
