659d39a3fd2009b5cf0b2116d67f0419ed4efa3c
Jonathan Adalin <jonathan.adalin@mail.mcgill.ca>
2017-11-13 19:51:18 -0500

    Initial commit for FFNN

---
+++ neural-network/nn.py
@@ -0,0 +1,16 @@
+# Feedforward Neural Network
+
+# Guidelines
+# A fully connected feedforward neural network (from lecture 14), trained by
+# backpropagation, where the network architecture (number of nodes / layers),
+# learning rate and termination are determined by cross-validation. This method
+# must be fully implemented by your team, and corresponding code submitted.
+
+# Note to TA
+# Please note that the below code was inspired by the 3-layer neural network
+# Python guide found here:
+# http://www.wildml.com/2015/09/implementing-a-neural-network-from-scratch/
+# as well as the COMP 551 lecture 14 notes found here:
+# http://cs.mcgill.ca/~jpineau/comp551/Lectures/14NeuralNets.pdf
+
+print "Hello, world."
